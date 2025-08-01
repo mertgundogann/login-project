cfb041c26c3afae23b2b6ae02752d36bdb6f4628
mertgundogann <merttgundogan@hotmail.com>
2025-08-01 08:00:12 +0300

    second commit

---
+++ src/components/Success.jsx
@@ -0,0 +1,13 @@
+// src/components/Success.jsx
+import React from 'react';
+
+const Success = () => {
+  return (
+    <div>
+      <h1>Giriş Başarılı!</h1>
+      <p>Hoşgeldiniz.</p>
+    </div>
+  );
+};
+
+export default Success;
